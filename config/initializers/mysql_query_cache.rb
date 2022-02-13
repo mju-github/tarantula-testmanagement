20a64c7cca0b1ec4769d63d6f706fae98e05fbc9
Meike <mju-github@jungebloed.net>
2022-02-14 00:37:34 +0100

    TAR-10 update project to make it run ith MySQL 5.7.20 and

--- config/initializers/mysql_query_cache.rb
+++ config/initializers/mysql_query_cache.rb
@@ -1,2 +1,2 @@
-
-ActiveRecord::Base.connection.execute("SET GLOBAL query_cache_type = 1;")
+# The query cache is deprecated as of MySQ 5.7.20 and is removed in MySQL 8.0
+#ActiveRecord::Base.connection.execute("SET GLOBAL query_cache_type = 1;")
